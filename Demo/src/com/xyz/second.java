cb5f479b73d3b74fdf5d045c66dc21064388e916
piyush-1908 <piyushdhananjayshete1234@gmail.com>
2025-08-04 10:00:45 +0530

    second

---
+++ Demo/src/com/xyz/second.java
@@ -0,0 +1,15 @@
+package com.xyz;
+
+public class second {
+	public static void main(String[] args) {
+		  char ch = 'a';
+		   System.out.println(ch);
+
+		   char ch1 = 65;
+		   System.out.println(ch1);
+
+		   int a= 'A';
+		   System.out.println(a);
+	}
+
+}
