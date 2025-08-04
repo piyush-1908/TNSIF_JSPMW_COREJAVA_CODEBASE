e8287375242324533214dfe4d5e38da928dac22e
piyush-1908 <piyushdhananjayshete1234@gmail.com>
2025-08-04 13:12:55 +0530

    this is day2 program

---
+++ Demo/src/com/xyz/IfDemo.java
@@ -0,0 +1,15 @@
+package com.xyz;
+
+public class IfDemo {
+	public static void main(String[] args) {
+		int age = 2;
+		if(age >= 18) {
+			System.out.println(" Eligible for voting ");
+		}else {
+			System.out.println(" Not eligible for voting");
+		}
+		
+	
+	}
+
+}
---
+++ Demo/src/com/xyz/nestedifelse.java
@@ -0,0 +1,27 @@
+package com.xyz;
+
+public class nestedifelse {
+	
+	public static void main(String[] args) {
+		int a= 10 , b= 20 ,c=5;
+		System.out.print("Your largest number is: ");
+		if(a>b) {
+			if(a>c) {
+				System.out.println(a);
+			}
+			else {
+				System.out.println(c);
+			}
+		}
+		else {
+			if(c>b) {
+				System.out.println(c);
+			}
+			else {
+				System.out.println(b);
+			}
+		}
+		 
+	}
+
+}
