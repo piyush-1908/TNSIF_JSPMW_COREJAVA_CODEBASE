226d88eb7cdf8b1d374863ae10f8e7bbd0e3428e
piyush-1908 <piyushdhananjayshete1234@gmail.com>
2025-08-04 10:06:19 +0530

    third

---
+++ Demo/src/com/xyz/third.java
@@ -0,0 +1,12 @@
+package com.xyz;
+
+public class third {
+	public static void main(String[] args) {
+		 int value1 = 9/2;
+	        System.out.println(value1);
+	        float value2 = 101f / 3f;
+	        System.out.println(value2);
+	        double value3 = 100d / 3d;
+	        System.out.println(value3);
+	}
+}
